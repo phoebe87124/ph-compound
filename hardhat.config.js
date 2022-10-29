c0678f28505eb2fa17d9233780d64ee59633ef9d
starbit_phoebe <phoebe.cheng@star-bit.io>
2022-10-29 16:11:00 +0800

    [add] config

---
+++ hardhat.config.js
@@ -0,0 +1,19 @@
+require("@nomicfoundation/hardhat-toolbox");
+
+/** @type import('hardhat/config').HardhatUserConfig */
+module.exports = {
+  // solidity: "0.8.17",
+  solidity: {
+    compilers: [
+      {
+        version: "0.8.17",
+        settings: {
+          optimizer: {
+            enabled: true,
+            runs: 200,
+          },
+        },
+      },
+    ]
+  },
+};
