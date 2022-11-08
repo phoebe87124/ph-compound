24f9911a57d87f0b953c697057ff2f0b7ca6b501
starbit_phoebe <phoebe.cheng@star-bit.io>
2022-11-08 13:19:05 +0800

    [edit] mint and redeem 改為 deployFixture 方式

--- test/mintAndRedeem.js
+++ test/mintAndRedeem.js
@@ -1,15 +1,16 @@
+const { loadFixture } = require("@nomicfoundation/hardhat-network-helpers");
 const { expect } = require('chai');
 
 describe("Week11", function(){
-  let comptroller, erc20, interestRateModel, cErc20, simplePriceOracle
-  it("Deploy CErc20, Comptroller and related contracts", async function(){
+  // Deploy CErc20, Comptroller and related contracts
+  async function deployFixture() {
     const comptrollerFactory = await ethers.getContractFactory("Comptroller")
-    comptroller = await comptrollerFactory.deploy()
+    const comptroller = await comptrollerFactory.deploy()
     await comptroller.deployed()
     
     // 需部署一個 CErc20 的 underlying ERC20 token，decimals 為 18
     const erc20Factory = await ethers.getContractFactory("phToken")
-    erc20 = await erc20Factory.deploy(
+    const erc20 = await erc20Factory.deploy(
       ethers.utils.parseUnits("10000000", 18),
       "phToken",
       "PH"
@@ -18,14 +19,14 @@ describe("Week11", function(){
 
     const interestRateModelFactory = await ethers.getContractFactory("WhitePaperInterestRateModel")
     // 將利率模型合約中的借貸利率設定為 0%
-    interestRateModel = await interestRateModelFactory.deploy(
+    const interestRateModel = await interestRateModelFactory.deploy(
       ethers.utils.parseUnits("0", 18),
       ethers.utils.parseUnits("0", 18),
     )
     await interestRateModel.deployed()
 
     const cErc20Factory = await ethers.getContractFactory("CErc20")
-    cErc20 = await cErc20Factory.deploy()
+    const cErc20 = await cErc20Factory.deploy()
     await cErc20.deployed()
 
     await cErc20["initialize(address,address,address,uint256,string,string,uint8)"](
@@ -37,20 +38,21 @@ describe("Week11", function(){
       "cPH",
       18      // CToken 的 decimals 皆為 18
     )
-  })
 
-  it ("set simplePriceOracle as comptroller oracle", async function(){
+    const [owner, user] = await ethers.getSigners();
+
     // 使用 SimplePriceOracle 作為 Oracle
     const simplePriceOracleFactory = await ethers.getContractFactory("SimplePriceOracle")
-    simplePriceOracle = await simplePriceOracleFactory.deploy()
+    const simplePriceOracle = await simplePriceOracleFactory.deploy()
     await simplePriceOracle.deployed()
     await expect(comptroller._setPriceOracle(simplePriceOracle.address))
     .to.emit(comptroller, "NewPriceOracle")
-  });
+
+    return { comptroller, erc20, interestRateModel, cErc20, simplePriceOracle, owner, user };
+  }
 
   it ("Should be able to mint/redeem with phToken", async function(){
-    const [owner, user] = await ethers.getSigners();
-    
+    const { comptroller, erc20, cErc20, user } = await loadFixture(deployFixture);
     // send user erc20
     await erc20.transfer(user.address, ethers.utils.parseUnits("10000", 18))
     // erc20 approve cErc20 to use
